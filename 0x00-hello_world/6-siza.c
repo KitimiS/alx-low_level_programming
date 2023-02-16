00709280d0fa7aa05896c3b809a7bf18a0fa11df
KitimiS <seshokakitimi@gmail.com>
2023-02-16 13:33:41 -0800

    6-siza.c

--- 0x00-hello_world/6-siza.c
+++ 0x00-hello_world/6-siza.c
@@ -11,14 +11,14 @@ int main(void)
 	long long int d;
 	float e;
 printf("Size of char: %lu byte(s)\n", (unsigned long)
-		sizeof(a));
+	sizeof(a));
 printf("Size of int: %lu byte(s)\n", (unsigned long)
-		sizeof(b));
+	sizeof(b));
 printf("Size of long int: %lu byte(s)\n", (unsigned long)
-		sizeof(c));
+	sizeof(c));
 printf("Size of long long int: %lu byte(s)\n", (unsigned long)
-		sizeof(d));
+	sizeof(d));
 printf("Size of float: %lu byte(s)\n", (unsigned long)
-		sizeof(e));
+	sizeof(e));
 return (0);
 }
