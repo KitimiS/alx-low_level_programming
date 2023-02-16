1af729e6d353548199ba15d02b929ca63ccd12d0
KitimiS <seshokakitimi@gmail.com>
2023-02-16 09:56:23 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,1 +1,16 @@
 #!/bin/bash
+#include <stdio.h>
+
+
+/**
+ * main - prints "Programming is like building a multilingual puzzle"
+ * return 0.
+ */
+
+int main(void)
+
+{
+	puts("\"Programming is like building a multilingual puzzle);
+
+	return(0);
+}
