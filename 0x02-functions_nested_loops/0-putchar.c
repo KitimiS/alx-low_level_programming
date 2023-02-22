5b14a9b14b942da88713d947988415345e3fcd36
KitimiS <seshokakitimi@gmail.com>
2023-02-22 06:11:08 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,8 @@
 	 * Return: 0
 	 */
 
-
-	int main(void)
-
+	int main(void);
 	{
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -20,5 +17,4 @@
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-
 	}
